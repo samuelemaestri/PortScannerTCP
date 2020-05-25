1c0dba154ae066f360698c0a20a124276c2e7817
samuelemaestri <65632854+samuelemaestri@users.noreply.github.com>
2020-05-25 11:22:56 +0200

    Add files via upload

---
+++ PortScannerTCP.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+scan() {
+  if [[ -z $1 || -z $2 ]]; then
+    echo "Usage: $0 <host> <port, ports, or port-range>"
+    return
+  fi
+
+  local host=$1
+  local ports={}
+  case $2 in
+    *-*)
+      IFS=- read start end <<< "$2"
+      for ((port=start; port <= end; port++)); do
+        ports+=($port)
+      done
+      ;;
+    *,*)
+      IFS=, read -ra ports <<< "$2"
+      ;;
+    *)
+      ports+=($2)
+      ;;
+  esac
+
+  for port in "${ports[@]}"; do
+  timeout 1 bash -c "echo >/dev/tcp/$host/$port" &&
+    echo "port $port is open" ||
+    echo "port $port is closed"
+  done
+}
